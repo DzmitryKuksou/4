92838b30ca9d7a12ebc4c81ecb5b39256879cbf8
dzm <vvv-rrrr@list.ru>
2017-07-07 16:29:56 +0300

    code

---
+++ NUnitNewtonRootMethod.Tests1/TestRootMethodNyoton.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using SearchRoots;
+
+namespace NUnitNewtonRootMethod.Tests1
+{
+    [TestFixture]
+    public class TestRootMethodNyoton
+    {
+        [TestCase(3, 0.00001, 2, 5, ExpectedResult = 1.73205)]
+        [TestCase(3, 0.001, 2, 3, ExpectedResult = 1.732)]
+        [TestCase(3, 0.000001, 2, 6, ExpectedResult = 1.732051)]
+        [TestCase(3, 0.0000001, 2, 7, ExpectedResult = 1.7320508)]
+        public double Test_SearchRootNewton(double number, double eps, int rPower, int round)
+        {
+
+            return Math.Round(SearchRoot.SearchRootNewton(number, eps, rPower), round);
+        }
+        [TestCase(3, 0.00001, -5)]
+        [TestCase(3, 1, -5)]
+        [TestCase(-3, 0.00001, 5)]
+        [TestCase(3, -0.00001, -5)]
+        public void Test_SearchRootNewton_ArgumentException(double number, double eps, int rPower)
+        {
+            Assert.Throws<ArgumentException>(() => SearchRoot.SearchRootNewton(number, eps, rPower));
+        }
+    }
+}
---
+++ SearchRootMethodNyoton/SearchRootMethodNyoton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchRoots
+{
+    public static class SearchRoot
+    {
+        /// <summary>
+        /// Checking of parameters
+        /// </summary>
+        /// <param name="number">number</param>
+        /// <param name="eps">epsilon</param>
+        /// <param name="rPower">Power of root</param>
+        private static void Checking(double number, double eps, int rPower)
+        {
+            if (number <= 0 || eps <= 0 || rPower <= 0) throw new ArgumentException("Numbers should be positive!");
+            if (eps > 1) throw new ArgumentException("Epsilon > 1");
+        }
+        /// <summary>
+        /// find root of number
+        /// </summary>
+        /// <param name="number">number</param>
+        /// <param name="eps">epsilon</param>
+        /// <param name="rPower">Power of root</param>
+        /// <returns>result</returns>
+        public static double SearchRootNewton(double number, double eps,  int rPower)
+        {
+            Checking(number, eps, rPower);
+            Math.Pow(3, 2);
+            double x1 = number / rPower;
+            double x2 = 1.0 / rPower * ((rPower - 1) * x1 + number / Math.Pow(x1, rPower - 1));
+            while (Math.Abs(x1 - x2) > eps) 
+            {
+                x1 = x2;
+                x2 = 1.0 / rPower * ((rPower - 1) * x1 + number / Math.Pow(x1, rPower - 1));
+            }
+            return x2;
+        }
+    }
+}
